3c2b1204ff060a9502ea23fed125913713062bcd
Deliar <52977770+deliarm@users.noreply.github.com>
2023-02-18 14:56:32 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,17 @@
 const connection = require( './connections/database');
 const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const dotenv = require("dotenv")
+const app = express()
+
 const authRoute = require("./routes/auth.js")
 
-dotenv.config()
-
-const app = express()
-app.use(express.json()).use(cors()).use(cookieParser());
-
-app.get("/", (req, res) => {
-    res.json({ msg: "Hello" });
-});
 
 
-
-  app.use("/api/", authRoute)
-  
-  
-  
 connection();
+
+app.use("/api/", authRoute)
+
+
+
 const port = process.env.port || 4000;
 app.listen(port, () => {
   console.log("server is running at port", port);
