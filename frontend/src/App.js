de1c5c35b692ca5a9490a2ac6a63dfda30e1df8d
Deliar <52977770+deliarm@users.noreply.github.com>
2023-02-18 15:22:09 -0700

    remove navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import login_page from "./pages/login_page"
+import login_page from "./pages/LoginPage"
 import HomePage from './pages/HomePage'
 import Navbar from './components/Navbar';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className='App'>
       <Router>
-        <Navbar/>
         <div className='content'>
           <Routes>
-            <Route exact path='/' element={<HomePage/>} />
+            <Route exact path='/' element={<LoginPage/>} />
+            <Route exact path='/home' element={<HomePage/>} />
           </Routes>
         </div>
       </Router>
