deef89f06835c5d2f253559eda378f6e3bb472eb
asadc24 <asad.choudhry1@ucalgary.ca>
2023-02-18 14:47:47 -0700

    finished user auth

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -42,7 +42,76 @@ const authController = {
         } catch (err) {
             return res.status(500).json({message: err.message})
         }
-    } 
+    },
+    login: async (req, res) => {
+        try {
+            const { username, password } = req.body
+    
+            const user = await Users.findOne({ username: username })
+                .populate("username fullname")
+            
+            if (!user) return res.status(400).json({ message: "This user does not exist"})
+            const isMatch = await bcrypt.compare(password, user.password)
+            if (!isMatch) return res.status(400).json({message: "Password incorrect"})
+
+
+            const refreshToken = createRefreshToken({ id: user._id })
+            const accessToken = createAccessToken({ id: user._id })
+
+            res.cookie('refreshtoken', refreshToken, {
+                httpOnly: true,
+                path: '/api/refresh_token',
+                maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+            })
+
+
+            res.json({
+                message: 'Login Success!',
+                accessToken,
+                user: {
+                    ...user._doc,
+                    password: ''
+                }
+            })
+
+        } catch(err) {
+            return res.status(500).json({ message: err.message })
+        }
+    },
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshtoken', { path: '/api/refresh_token' })
+            return res.json({ message: "Logged out!" })
+        } catch (err) {
+            return res.status(500).json({ message: err.message })
+        }
+    },
+    generateAccessToken: async (req, res) => {
+        try {
+            const rf_token = req.cookies.refreshtoken
+            if (!rf_token) return res.status(400).json({ message: "Please login now." })
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async (err, result) => {
+                if (err) return res.status(400).json({ message: "Please login now." })
+
+                const user = await Users.findById(result.id).select("-password")
+                    .populate("username fullname")
+
+                if (!user) return res.status(400).json({ message: "This user does not exist." })
+
+                const access_token = createAccessToken({ id: result.id })
+
+                res.json({
+                    access_token,
+                    user
+                })
+            })
+
+        } catch (err) {
+            return res.status(500).json({ message: err.message })
+        }
+    }
+
 
 }
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,9 @@ const registerMiddleware = require('../middleware/signup.js');
 const authRoute = Router()
 
 authRoute.post('/register',  registerMiddleware, authController.register);
+authRoute.post('/login', authController.login)
+authRoute.post('/logout', authController.logout)
+authRoute.post('/refresh_token', authController.generateAccessToken)
 
 module.exports = authRoute
 
